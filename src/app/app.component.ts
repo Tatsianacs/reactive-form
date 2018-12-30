b921d1d284f55d1e30f45e13071d0c47cbd006b7
tania <strekasia@tut.by>
2018-12-30 16:06:48 +0300

    feat: add validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
 
 @Component({
     selector: 'app-root',
@@ -9,12 +10,13 @@ import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 export class AppComponent implements OnInit {
     genders = ['male', 'female'];
     signupForm: FormGroup;
+    forbiddenUsernames = ['Chris', 'Anna'];
 
     ngOnInit() {
         this.signupForm = new FormGroup({
             'userData': new FormGroup({
-                'username': new FormControl(null, Validators.required),
-                'email': new FormControl(null, [Validators.required, Validators.email]),
+                'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+                'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
 
             }),
             'gender': new FormControl('male'),
@@ -23,11 +25,31 @@ export class AppComponent implements OnInit {
     }
 
     onSubmit() {
-        console.log('form' + this.signupForm);
+        console.log(this.signupForm);
     }
 
     onAddHobby() {
         const control = new FormControl(null, Validators.required); // no default value
         (<FormArray>this.signupForm.get('hobbies')).push(control);
+    }
+
+    forbiddenNames(control: FormControl): { [s: string]: boolean } {
+        if (this.forbiddenUsernames.includes(control.value)) {
+            return {'nameIsForbidden': true};
+        }
+        return null;
+    }
+
+    forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+        const promise = new Promise<any>((resolve, reject) => {
+            setTimeout(() => {
+                if (control.value === 'test@test.com') {
+                    resolve({'emailIsForbidden': true});
+                } else {
+                    resolve(null);
+                }
+            }, 1500);
+        });
+        return promise;
     }
 }
